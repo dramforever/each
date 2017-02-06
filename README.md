d5b409a9615995ef7867630261612757d10a7fb3
dramforever <catmimiao@me.com>
2017-02-06 23:14:35 +0800

    Full rewrite to use do-notation

--- README.md
+++ README.md
@@ -2,23 +2,65 @@
 
 Inspired by [the Scala library of the same name](https://github.com/ThoughtWorksInc/each),
 each is a Template Haskell library that transforms expressions containing
-invocations of impure subexpressions into calls to `fmap`, `<*>`, `join`,
-etc. Just mark your impure subexpressions with `bind` or `~!` and they will be
-called appropriately, as in this small demo:
+invocations of impure subexpressions into do-notation. Just mark your impure
+subexpressions with `bind` or `~!` and they will be called appropriately,
+as in this small demo:
 
     ghci> $(each [| "Hello, " ++ (~! getLine) |])
     World              <--[keyboard input]
     "Hello, World"
 
-In this case, the code is translated into `fmap ((++) "Hello, ") getLine`
+With the `ApplicativeDo` GHC extension, calls to `fmap` and `<*>` will be
+arranged so that you don't need to worry if you use, say, Haxl and needs
+`Applicative` for parallelism.
 
-We currently have support for
+Most constructs where this would make things much more simpler are already
+supported. In particular, these are okay:
 
-* Normal function application like `f x y`
-* Infix operator application like `x + y`, including sections like `(+ y)`
-* Type signatures like `x :: t`
+- Nested `bind`s.
+- Branching constructs, even if the branches themselves uses `bind`. The
+generated `do`-notation will generally match imperative intuition.
 
-Support for more constructs is coming.
+These are some quirks:
+
+- `let` expressions are evaluated sequentially. `each` currently lacks support
+for detecting pure `let` expressions.
+- `where` is not implemented.
+- Parameters to lambda functions may not be used impurely. This is acceptable,
+but the error message may be confusing:
+
+        ghci> $(each [| (\x -> bind x) |])
+
+        <interactive>:25:3: error:
+        • The exact Name ‘x_acBv’ is not in scope
+        Probable cause: you used a unique Template Haskell name (NameU),
+        perhaps via newName, but did not bind it
+        If that's it, then -ddump-splices might be useful
+        • In the untyped splice: $(each [| (\ x -> bind x) |])
+
+  Also, `bind`s in the lambda will be run when the lambda is *constructed*,
+not when it's called.
+- `PatternGuard`, `LambdaCase` and a few other extensions (uncertain) are not
+yet implemented.
+
+If you find something wrong, or really want some feature, feel free to leave an
+issue.
+
+## How it works
+
+The basic structure of an `each` block is this:
+
+    $(each [| ... |])
+
+Inside of this block, three (interchangable) ways are used to mark impure
+subexpressions:
+
+- `bind expr`
+- `bind $ expr`
+- `(~! expr)`
+
+`do`-notation is generated according to left-to-right order, and branching is
+handled.
 
 ## More demos
 
--- src/Each/Transform.hs
+++ src/Each/Transform.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
 {-# LANGUAGE LambdaCase #-}
 
 -----------------------------------------------------------------------------
@@ -14,102 +14,180 @@
 -- An internal module where most of the real transformation goes on.
 -----------------------------------------------------------------------------
 
-module Each.Transform
-    ( transform
-    , Env (..)
-    , Result (..)
-    ) where
+module Each.Transform where
 
 import Control.Applicative
-import Control.Monad.Reader
+import Control.Monad
+import Data.DList (DList, singleton, toList)
 import Data.Monoid
+
 import Language.Haskell.TH
-
-
--- The following modules are imported for use in splices
-import qualified Control.Monad
-import qualified Data.Functor
 
 import qualified Each.Invoke
 
-data Result
-    = Pure ExpQ -- ^ This subexpression does not invoke bind, i.e. is pure
-    | Bind ExpQ -- ^ This subexpression contains invocations of bind
+-- | A writer monad where the empty case is distinguished.
+data Result a
+    = Impure (DList Stmt) a -- ^ Invariant: the bind list is not empty
+    | Pure a
 
-data Env
-    = Env
-    { envType :: Maybe TypeQ
-    --, envLocals :: M.Map Name Result
-    }
+instance Functor Result where
+    f `fmap` Impure bs x = Impure bs (f x)
+    f `fmap` Pure a = Pure (f a)
 
-type M = ReaderT Env Q
+instance Applicative Result where
+    pure = Pure
+    Pure f <*> Pure x = Pure (f x)
+    Pure f <*> Impure xbs x = Impure xbs (f x)
+    Impure fbs f <*> Pure x = Impure fbs (f x)
+    Impure fbs f <*> Impure xbs x = Impure (fbs <> xbs) (f x)
 
-transform :: Exp -> Env -> Q Result
-transform ex env = runReaderT (transform' ex) env
+instance Monad Result where
+    Impure bs x >>= k = case k x of
+        Impure ks r -> Impure (bs <> ks) r
+        Pure r -> Impure bs r
+    Pure x >>= k = k x
 
-transform' :: Exp -> M Result
+addBind :: Name -> Exp -> Result ()
+addBind n e = Impure (singleton (BindS (VarP n) e)) ()
+
+-- | Invoke an 'each' block
+each :: ExpQ -> ExpQ
+each inp = generate <$> (inp >>= transform)
+
+generate :: Result Exp -> Exp
+generate (Pure x) = AppE (VarE 'Control.Applicative.pure) x
+generate (Impure xs x) = DoE $ toList (xs <> singleton (
+    NoBindS (AppE (VarE 'Control.Monad.return) x)))
+
+transform :: Exp -> Q (Result Exp)
 -- Detecting and processing invocations of bind
-transform' (InfixE Nothing (VarE v) (Just x))
+transform (InfixE Nothing (VarE v) (Just x))
     | v == '(Each.Invoke.~!) = impurify x
-transform' (AppE (VarE v) x)
+transform (AppE (VarE v) x)
     | v == 'Each.Invoke.bind = impurify x
-transform' (InfixE (Just (VarE vf)) (VarE vo) (Just x))
+transform (InfixE (Just (VarE vf)) (VarE vo) (Just x))
     | vf == 'Each.Invoke.bind && vo == '(Prelude.$) = impurify x
 
-transform' (VarE vn) = pure $ Pure (varE vn)
-transform' (ConE cn) = pure $ Pure (conE cn)
-transform' (LitE lit) = pure $ Pure (litE lit)
+transform (VarE n) = pure $ pure (VarE n)
+transform (ConE n) = pure $ pure (ConE n)
+transform (LitE l) = pure $ pure (LitE l)
 
-transform' (AppE f x) =
-        liftA2 go (transform' f) (transform' x)
+transform (AppE f x) =
+    liftA2 (liftA2 AppE) (transform f) (transform x)
+
+transform (InfixE lhs mid rhs) = do
+    tl <- traverse transform lhs
+    tm <- transform mid
+    tr <- traverse transform rhs
+    pure (liftA3 InfixE (sequence tl) tm (sequence tr))
+
+-- TODO Maybe add checks to ensure that the arguments aren't used impurely?
+transform (LamE ps x) = fmap (LamE ps) <$> transform x
+
+transform (TupE ps) = fmap TupE . sequence <$> (traverse transform ps)
+
+transform (CondE c t f) = do
+    tc <- transform c
+    tt <- transform t
+    tf <- transform f
+    case liftA2 (,) tt tf of
+        Pure (et, ef) -> pure $ (\z -> CondE z et ef) <$> tc
+        res -> do
+            var <- newName "bind"
+            pure $ do
+                ec <- tc
+                addBind var (CondE ec (generate tt) (generate tf))
+                pure (VarE var)
+
+transform (MultiIfE bs) = case desugarMultiIf bs of
+        Right x -> transform x
+        Left err -> fail err
     where
-        go (Pure ef) (Pure ex) = Pure [| $(ef) $(ex) |]
-        go (Pure ef) (Bind ex) = Bind [| Data.Functor.fmap $(ef) $(ex) |]
-        go (Bind ef) (Pure ex) = Bind [| Data.Functor.fmap ($ $(ex)) $(ef) |]
-        go (Bind ef) (Bind ex) = Bind [| (Control.Applicative.<*>) $(ef) $(ex) |]
+        desugarMultiIf :: [(Guard, Exp)] -> Either String Exp
+        desugarMultiIf [] = pure (AppE
+            (VarE 'Prelude.error)
+            (LitE $ StringL errNonExhaustiveGuard))
+        desugarMultiIf ((NormalG c, t) : bs) = go <$> desugarMultiIf bs
+            where go f = CondE c t f
+        desugarMultiIf ((PatG _, _) : _) =
+            Left errPatternGuard
 
-transform' (InfixE Nothing op Nothing) =
-    transform' op
+transform (LetE [] e) = transform e
+transform (LetE (ValD p v [] : ds) e) =
+    transform (CaseE (bodyToExp v) [Match p (NormalB $ LetE ds e) []])
 
-transform' (InfixE (Just lhs) op Nothing) =
-    transform' (AppE op lhs)
+transform (LetE (ValD _ _ _ : _) _) = fail errWhere
+transform (LetE _ _) = fail errComplexLet
 
-transform' (InfixE Nothing op (Just rhs)) =
-    lift [| Prelude.flip $(pure op) $(pure rhs) |] >>= transform'
-
-transform' (InfixE (Just lhs) op (Just rhs)) =
-    transform' (AppE (AppE op lhs) rhs)
-
-transform' (SigE x ty) = transform' x >>= \tx -> case tx of
-    Pure ex -> pure $ Pure [| $(ex) :: $(pure ty) |]
-
-    Bind ex -> do
-        Env { envType = mety } <- ask
-        case mety of
+transform (CaseE s ma) = do
+        ts <- transform s
+        tm <- traverse transformMatch ma
+        case traverse getPureMatch tm of
+            Just pes -> pure $ (\z -> CaseE z (toMatch <$> pes)) <$> ts
             Nothing -> do
-                ok <- lift $ isExtEnabled PartialTypeSignatures
-                if ok
-                    -- Try PartialTypeSignatures
-                    then pure $ Bind [| $(ex) :: $(pure WildCardT) $(pure ty) |]
-                    else fail errSig
-            Just ety ->
-                -- Use the supplied context type
-                pure $ Bind [| $(ex) :: $(ety) $(pure ty) |]
-
-transform' x = fail (errUnsupportedSyntax x)
-
-impurify :: Exp -> M Result
-impurify x =
-        go <$> transform' x
+                var <- newName "bind"
+                pure $ do
+                    es <- ts
+                    addBind var (CaseE es (generateMatch <$> tm))
+                    pure (VarE var)
     where
-        go (Pure e) = Bind e
-        go (Bind e) = Bind [| Control.Monad.join $(e) |]
+        generateMatch :: (Pat, Result Exp) -> Match
+        generateMatch (p, e) = toMatch (p, generate e)
 
-errUnsupportedSyntax :: Exp -> String
-errUnsupportedSyntax x = "each: Unsupported syntax in " <> pprint x
+        toMatch :: (Pat, Exp) -> Match
+        toMatch (p, e) = Match p (NormalB e) []
 
-errSig :: String
-errSig =
-    "each: Using type signatures on expressions containing 'bind' requires \
-    \specifying the context type using 'eachWith', or enabling \
-    \-XPartialTypeSignatures."
+        getPureMatch :: (Pat, Result Exp) -> Maybe (Pat, Exp)
+        getPureMatch (pat, Pure e) = Just (pat, e)
+        getPureMatch _ = Nothing
+
+        transformMatch :: Match -> Q (Pat, Result Exp)
+        transformMatch (Match pat body []) =
+            (\x -> (pat, x)) <$> transform (bodyToExp body)
+        transformMatch _ = fail errWhere
+
+transform (ArithSeqE z) =
+    fmap ArithSeqE <$> case z of
+        FromR a -> fmap FromR <$> transform a
+        FromThenR a b -> liftA2 (liftA2 FromThenR) (transform a) (transform b)
+        FromToR a b -> liftA2 (liftA2 FromToR) (transform a) (transform b)
+        FromThenToR a b c -> liftA3 (liftA3 FromThenToR)
+            (transform a) (transform b) (transform c)
+
+transform (ListE xs) = fmap ListE . sequence <$> (traverse transform xs)
+
+transform (SigE e t) = fmap (\te -> SigE te t) <$> transform e
+
+transform (RecConE name fes) =
+    fmap (RecConE name) . sequence
+    <$> (traverse transformFieldExp fes)
+
+transform (RecUpdE x fes) =
+    liftA2 (liftA2 RecUpdE)
+    (transform x)
+    (sequence <$> traverse transformFieldExp fes)
+
+transform (UnboundVarE n) = pure $ pure (UnboundVarE n)
+
+transform x = fail (errUnsupported <> pprint x)
+
+bodyToExp :: Body -> Exp
+bodyToExp (NormalB x) = x
+bodyToExp (GuardedB x) = MultiIfE x
+
+transformFieldExp :: FieldExp -> Q (Result FieldExp)
+transformFieldExp (nm, e) = fmap (\x -> (nm, x)) <$> transform e
+
+impurify :: Exp -> Q (Result Exp)
+impurify e = liftA2 go (transform e) (newName "bind")
+    where
+        go te nm = te >>= \z -> VarE nm <$ addBind nm z
+
+errNonExhaustiveGuard, errUnsupported,
+    errPatternGuard, errWhere, errComplexLet :: String
+
+errNonExhaustiveGuard = "Non-exhaustive guard"
+errUnsupported = "Unsupported syntax in: "
+errPatternGuard = "Pattern guards are not supported"
+errWhere = "'where' is not supported"
+errComplexLet = "Only declarations like 'pattern = value' are supported in let"
